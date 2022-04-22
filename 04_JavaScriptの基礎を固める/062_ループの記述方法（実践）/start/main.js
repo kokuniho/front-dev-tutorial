eb42c91522478e752240ac372cf77be2a7feae1f
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-04-22 19:26:11 +0900

    roup3

---
+++ 04_JavaScriptの基礎を固める/062_ループの記述方法（実践）/start/main.js
@@ -0,0 +1,36 @@
+const todos = [
+  {
+    id: 1,
+    title: 'Go to school',
+    completed: true
+  },
+  {
+    id: 2,
+    title: 'Go to library',
+    completed: false
+  },
+  {
+    id: 3,
+    title: 'Go shopping',
+    completed: true
+  }
+
+]
+
+// for(let i = 0; i < todos.length; i++){
+//   let todo = todos[i];
+//   if(todo.completed === true){
+//       console.log(i, todo.title);
+//   }
+// }
+
+// for(let i in todos){
+//   let todo = todos[i];
+//   if(todo.completed === true){
+//       console.log(i, todo.title);
+//   }
+  for(let todo of todos){
+    if(todo.completed === true){
+        console.log(todo.title);
+    }
+}
