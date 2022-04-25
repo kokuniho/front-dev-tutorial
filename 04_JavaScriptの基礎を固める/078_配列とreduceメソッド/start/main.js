6d2a51dcd689a2f2a461269bde7dd63a8eb782a7
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-04-25 17:56:08 +0900

    reduce

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,22 @@
 const arry = [1,2,3,4,5];
+
+arry.reduce(function(accu, curr){
+    console.log(accu, curr);
+    return accu + curr;
+}
+,10);
+
+//currには配列の値が渡ってくる。
+
+const str = 'animation';
+const strArry = str.split('');
+
+console.log(strArry);
+
+const result = strArry.reduce((accu, curr) => {
+  return `${accu}<${curr}>`;
+}, "")
+
+//return `${accu}<${curr}>`; 変数は波括弧でかこう。文字列（＜＞）はそのまま。足し算の場合。
+
+console.log(result);
