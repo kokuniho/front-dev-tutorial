d68fc8bb36805707a26bd42c5cafc6ab7eddc5ba
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-04-25 14:18:03 +0900

    allow

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,20 @@
+// function hello(name) {
+//   console.log('hello' + name);
+// }
+//引数が２つ以上だと（）必要　
+// const hello = (name, age) => console.log('hello' + name + age);
+// const hello = name => console.log('hello' + name)
+
+// hello('code mafia');
+// hello('codemafia 2', 20);
+
+const hello = (name, age) => 40;
+
+const arry = [1,2,3,4,5,6];
+// arry.forEach(function(value) {
+//   console.log(value);
+// })
+//上記を書き換え
+arry.forEach(value => console.log(value));
+
+
