8eea834b0ab05b37f3c158c02480b3c12ad0559b
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-05-07 19:53:57 +0900

    obj_class

---
+++ 04_JavaScriptの基礎を固める/113_classとオブジェクト/start/main.js
@@ -0,0 +1,22 @@
+const obj = {
+  first_name: 'Mafia',
+  last_name: 'code',
+  printFullName: function(){
+    console.log('hello');
+  }
+}
+
+class MyObj {
+  constructor() {
+    this.first_name = 'Mafia';
+    this.last_name = 'Code';
+  }
+  printFullName() {
+    console.log('hello');
+  }
+}
+const obj2 = new MyObj();
+
+// console.log(obj.first_name);
+obj.printFullName();
+obj2.printFullName();
